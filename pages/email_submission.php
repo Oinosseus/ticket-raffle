f6ec9bf03032864e086ab02c46caf6064edceab3
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2017-03-02 23:45:54 +0100

    renaming in email submissions

--- pages/email_submission.php
+++ pages/email_submission.php
@@ -6,59 +6,59 @@ $UserVerifKey = "";
 if (isset($_REQUEST['UserVerificationKey'])) $UserVerifKey = trim($_REQUEST['UserVerificationKey']);
 
 // drawing id
-$DrawingId = 0;
-if (isset($_REQUEST['DrawingId'])) $DrawingId = intval($_REQUEST['DrawingId']);
+$ParticipationId = 0;
+if (isset($_REQUEST['ParticipationId'])) $ParticipationId = intval($_REQUEST['ParticipationId']);
 
 // try to find drawing
-$drawing = Null;
-foreach ($DB->getDrawings() as $d) {
-    if ($d->getId() == $DrawingId) {
-        $drawing = $d;
+$participation = Null;
+foreach ($DB->getParticipations() as $d) {
+    if ($d->getId() == $ParticipationId) {
+        $participation = $d;
         break;
     }
 }
 
 // drawing not found
-if ($drawing == Null) {
+if ($participation == Null) {
     echo '<div class="message error">Ziehung konnte nicht gefunden werden!</div>';
 
 // invalid key
-} else if ($drawing->getUserVerificationKey() != $UserVerifKey) {
+} else if ($participation->getUserVerificationKey() != $UserVerifKey) {
     echo '<div class="message error">Ung&uuml;ltiger Best&auml;tigungscode!</div>';
 
 // drawing and key are valid
 } else {
 
     // make existing key invalid
-    $drawing->createUserVerificationKey();
-    $drawing->save();
+    $participation->createUserVerificationKey();
+    $participation->save();
 
     // enter drawing
-    if ($drawing->getState() == Drawing::STATE_ENTRY_REQUESTED) {
+    if ($participation->getState() == Participation::STATE_ENTRY_REQUESTED) {
 
         // raffle is not in committed state
-        if ($drawing->getRaffle()->getState() != Raffle::STATE_OPEN) {
+        if ($participation->getRaffle()->getState() != Raffle::STATE_OPEN) {
             echo '<div class="message error">An der Ziehung kann nicht mehr teilgenommen werden!</div>';
 
         // raffle is in committed state
         } else {
-            $drawing->setState(Drawing::STATE_ENTRY_ACCEPTED);
-            $drawing->save();
-            echo '<div class="message success">Sie sind der Ziehung ' . $drawing->getRaffle()->getName() . ' beigetreten.</div>';
+            $participation->setState(Participation::STATE_ENTRY_ACCEPTED);
+            $participation->save();
+            echo '<div class="message success">Sie sind der Ziehung ' . $participation->getRaffle()->getName() . ' beigetreten.</div>';
         }
 
     // decline drawing
-    } else if ($drawing->getState() == Drawing::STATE_DECLINE_REQUESTED) {
+    } else if ($participation->getState() == Participation::STATE_DECLINE_REQUESTED) {
 
         // raffle is not in committed state
-        if ($drawing->getRaffle()->getState() != Raffle::STATE_OPEN) {
+        if ($participation->getRaffle()->getState() != Raffle::STATE_OPEN) {
             echo '<div class="message error">Aus der Ziehung kann nicht mehr ausgetreten werden!</div>';
 
         // raffle is in committed state
         } else {
-            $drawing->setState(Drawing::STATE_DECLINE_ACCEPTED);
-            $drawing->save();
-            echo '<div class="message success">Sie sind aus der Ziehung ' . $drawing->getRaffle()->getName() . ' ausgetreten.</div>';
+            $participation->setState(Participation::STATE_DECLINE_ACCEPTED);
+            $participation->save();
+            echo '<div class="message success">Sie sind aus der Ziehung ' . $participation->getRaffle()->getName() . ' ausgetreten.</div>';
         }
     }
 }
