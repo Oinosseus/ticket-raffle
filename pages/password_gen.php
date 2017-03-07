4e9495b38c61f1c12636edcf4df784c7bf18ffcd
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2017-03-07 21:55:22 +0100

    adopting quotes to match config.php

--- pages/password_gen.php
+++ pages/password_gen.php
@@ -5,8 +5,8 @@
     <?php
         if (isset($_POST['generated_password'])) {
             $pw_hash = password_hash($_POST['generated_password'], PASSWORD_DEFAULT);
-            echo "Generated password: \"$pw_hash\"<br>";
-            echo "password algorithm: \"" . password_get_info($pw_hash)['algoName'] . "\"<br>";
+            echo "Generated password: '$pw_hash'<br>";
+            echo "password algorithm: " . password_get_info($pw_hash)['algoName'] . "<br>";
         }
     ?>
 </form>
