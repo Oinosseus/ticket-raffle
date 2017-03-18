00e02feb566fde9da0b8796578b2a7bbc96e1df8
Thomas Weinhold <Thomas.Weinhold@stratoinos.de>
2017-03-18 20:11:03 +0100

    show next percentage of participants

--- pages/participants.php
+++ pages/participants.php
@@ -15,16 +15,22 @@
             <th>Email</th>
             <th>Particip.</th>
             <th>Wins</th>
+            <th>Next Weight</th>
         </tr>
 
     <?php
         foreach ($participants as $pt) {
 
+            $participations = $pt->countParticipations();
+            $wins           = $pt->countWins();
+            $next_weight    = number_format( 100 * ($participations + 1 - $wins) / ($participations + 1));
+
             echo '<tr>';
             echo '    <td>' . $pt->getId() . '</td>';
             echo '    <td>' . $pt->getEmail() . '</td>';
-            echo '    <td>' . $pt->countParticipations() . '</td>';
-            echo '    <td>' . $pt->countWins() . '</td>';
+            echo '    <td>' . $participations . '</td>';
+            echo '    <td>' . $wins . '</td>';
+            echo '    <td>' . $next_weight . '%</td>';
             echo '<tr>';
         }
     ?>
